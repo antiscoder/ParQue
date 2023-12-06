224b14f28d2a2314e676b313178c5ae604a28a42
Karel Fernandez <karel.fernandez@sjsu.edu>
2023-12-06 02:12:51 -0800

    error handling for the parking guides can only select one parking spot

--- app/src/Components/ParkingGuideSouth/ParkingGuideSouth.jsx
+++ app/src/Components/ParkingGuideSouth/ParkingGuideSouth.jsx
@@ -66,9 +66,18 @@ export const ParkingMap = () => {
 
 const ParkingGuideSouth = () => {
   const navigate = useNavigate();
+  const [selectedSpot, setSelectedSpot] = useState(null); // Track the selected parking spot
 
   const handleParkedClicked = () => {
-    navigate('/parkingduration');
+    const occupiedSpotCount = south_parking.getParkingSpots.reduce((count, spot) => count + spot, 0);
+  
+    if (occupiedSpotCount < 1) {
+      alert('Please only select one parking spot.')
+    } else if (occupiedSpotCount > 1) {
+      alert('Please select a parking spot.');
+    } else {
+      navigate('/parkingduration');
+    }
   };
 
   return (
